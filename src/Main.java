61229eb8a9c23cb539aebef36b40ec23ded37545
PERTE BOGDAN <bogdi_perte98@yahoo.com>
2022-10-07 11:37:21 +0300

    TEMA INITIERE JAVA LAB. 8

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("Bogdan");
+        System.out.println(10+38);
+        float x= 10;
+        float y= 3;
+        System.out.println(x/y);
+        int a= -5 + 8 * 6;
+        int b= (55+9) % 9;
+        int c= 20 + -3*5 / 8;
+        int d= 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
